2ef4c2fa5f065df0cad1167a0ffc408cba7b1d95
JuanCarlosOrtega <juancarloslinux@gmail.com>
2015-11-23 11:11:17 +0100

    Add some provider configuration
    Add some provider parser configuration

--- newsme-api/src/main/java/com/jcsoftware/newsmeapi/repository/InMemoryParserConfigurationDao.java
+++ newsme-api/src/main/java/com/jcsoftware/newsmeapi/repository/InMemoryParserConfigurationDao.java
@@ -1,5 +1,6 @@
 package com.jcsoftware.newsmeapi.repository;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,6 +13,78 @@ public class InMemoryParserConfigurationDao implements ParserConfigurationDao {
 	
 	private List<ParserConfiguration> parserConfigurations;
 
+	public InMemoryParserConfigurationDao() {
+		parserConfigurations = new ArrayList<ParserConfiguration>();
+		
+		ParserConfiguration elMundoParserConfiguration = new ParserConfiguration();
+		elMundoParserConfiguration.setItemTag("item");
+		elMundoParserConfiguration.setItemTypeSelector("elements");
+		elMundoParserConfiguration.setTitleTag("title");
+		elMundoParserConfiguration.setTitleTypeSelector("text");
+		elMundoParserConfiguration.setCreatorTag("dc|creator");
+		elMundoParserConfiguration.setCreatorTypeSelector("text");
+		elMundoParserConfiguration.setLinkTag("link");
+		elMundoParserConfiguration.setLinkTypeSelector("text");
+		elMundoParserConfiguration.setDescriptionTag("media|description");
+		elMundoParserConfiguration.setDescriptionTypeSelector("text");
+		elMundoParserConfiguration.setDateTag("pubdate");
+		elMundoParserConfiguration.setDateTypeSelector("text");
+		elMundoParserConfiguration.setImageTag("media|content");
+		elMundoParserConfiguration.setImageTagTypeSelector("attr");
+				
+		ParserConfiguration abcParserConfiguration = new ParserConfiguration();
+		abcParserConfiguration.setItemTag("item");
+		abcParserConfiguration.setItemTypeSelector("elements");
+		abcParserConfiguration.setTitleTag("title");
+		abcParserConfiguration.setTitleTypeSelector("text");
+		abcParserConfiguration.setCreatorTag("dc|creator");
+		abcParserConfiguration.setCreatorTypeSelector("text");
+		abcParserConfiguration.setLinkTag("link");
+		abcParserConfiguration.setLinkTypeSelector("text");
+		abcParserConfiguration.setDescriptionTag("description");
+		abcParserConfiguration.setDescriptionTypeSelector("text");
+		abcParserConfiguration.setDateTag("pubdate");
+		abcParserConfiguration.setDateTypeSelector("text");
+		abcParserConfiguration.setCategoryTag("category");
+		abcParserConfiguration.setCategoryTypeSelector("elements");
+		
+		ParserConfiguration elPaisParserConfiguration = new ParserConfiguration();
+		elPaisParserConfiguration.setItemTag("item");
+		elPaisParserConfiguration.setItemTypeSelector("elements");
+		elPaisParserConfiguration.setTitleTag("title");
+		elPaisParserConfiguration.setTitleTypeSelector("text");
+		elPaisParserConfiguration.setCreatorTag("dc|creator");
+		elPaisParserConfiguration.setCreatorTypeSelector("text");
+		elPaisParserConfiguration.setLinkTag("link");
+		elPaisParserConfiguration.setLinkTypeSelector("text");
+		elPaisParserConfiguration.setDescriptionTag("description");
+		elPaisParserConfiguration.setDescriptionTypeSelector("text");
+		elPaisParserConfiguration.setDateTag("pubdate");
+		elPaisParserConfiguration.setDateTypeSelector("text");
+		elPaisParserConfiguration.setImageTag("enclosure");
+		elPaisParserConfiguration.setImageTagTypeSelector("attr");
+		elPaisParserConfiguration.setCategoryTag("category");
+		elPaisParserConfiguration.setCategoryTypeSelector("elements");
+		
+		ParserConfiguration elPeriodicoParserConfiguration = new ParserConfiguration();
+		elPeriodicoParserConfiguration.setItemTag("item");
+		elPeriodicoParserConfiguration.setItemTypeSelector("elements");
+		elPeriodicoParserConfiguration.setTitleTag("title");
+		elPeriodicoParserConfiguration.setTitleTypeSelector("text");
+		elPeriodicoParserConfiguration.setLinkTag("link");
+		elPeriodicoParserConfiguration.setLinkTypeSelector("text");
+		elPeriodicoParserConfiguration.setDateTag("pubdate");
+		elPeriodicoParserConfiguration.setDateTypeSelector("text");
+		elPeriodicoParserConfiguration.setDescriptionTag("description");
+		elPeriodicoParserConfiguration.setDescriptionTypeSelector("text");
+		
+		
+		parserConfigurations.add(elMundoParserConfiguration);
+		parserConfigurations.add(abcParserConfiguration);
+		parserConfigurations.add(elPaisParserConfiguration);
+		parserConfigurations.add(elPeriodicoParserConfiguration);
+	}
+	
 	@Override
 	public ParserConfiguration getParserConfigurationByProviderId(long id) {
 		for (ParserConfiguration p : parserConfigurations) {
